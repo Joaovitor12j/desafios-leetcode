704bd09d7fbf896cb313748f92bf500981bbc8e2
João Vitor <joaovitor12j@gmail.com>
2026-07-04 19:25:51 -0300

    feat: leetcode php 171
    - Desafio 171 Excel Sheet resolvido

---
+++ PHP/171-excel-sheet-column-number/SolutionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LeetCode\PHP\P171;
+
+use PHPUnit\Framework\Attributes\RunClassInSeparateProcess;
+use PHPUnit\Framework\TestCase;
+use Solution;
+
+#[RunClassInSeparateProcess]
+final class SolutionTest extends TestCase
+{
+    private Solution $solution;
+
+    protected function setUp(): void
+    {
+        require_once __DIR__ . '/solution.php';
+
+        $this->solution = new Solution();
+    }
+
+    public function testExampleOne(): void
+    {
+        self::assertSame(1, $this->solution->titleToNumber('A'));
+    }
+
+    public function testExampleTwo(): void
+    {
+        self::assertSame(28, $this->solution->titleToNumber('AB'));
+    }
+
+    public function testExampleThree(): void
+    {
+        self::assertSame(701, $this->solution->titleToNumber('ZY'));
+    }
+
+    public function testSingleLetterBoundaries(): void
+    {
+        self::assertSame(1, $this->solution->titleToNumber('A'));
+        self::assertSame(26, $this->solution->titleToNumber('Z'));
+    }
+
+    public function testFirstTwoLetterColumn(): void
+    {
+        self::assertSame(27, $this->solution->titleToNumber('AA'));
+    }
+
+    public function testMaximumLengthWithinConstraints(): void
+    {
+        self::assertSame(2147483647, $this->solution->titleToNumber('FXSHRXW'));
+    }
+}
